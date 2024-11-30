712394b3b4355570de015d1576f102cc14b9c297
calaf <117057567+ca1af@users.noreply.github.com>
2024-11-30 16:24:14 +0900

    feat(Lec1Main) : 변수 선언 및 널처리
    1. var : 가변 변수
    2. val : final 변수
    3. type 은 선택적으로 명시 가능, 하지만 nullable 한 경우 반드시 명시 후 '?' 붙여야함

---
+++ src/main/java/com/lannstark/lec01/Lec1MainKt.kt
@@ -0,0 +1,12 @@
+package com.lannstark.lec01
+
+fun main() {
+    var modifiable = 10;
+    modifiable = 20;
+
+    val unmodifiable = 10;
+
+    // nullable
+    var foo :Long?
+    val person = Person("foo")
+}
