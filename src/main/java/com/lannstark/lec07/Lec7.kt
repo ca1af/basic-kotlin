95741b3db627407d9c912e38a96ef377bab32dbb
calaf <117057567+ca1af@users.noreply.github.com>
2024-12-01 12:54:49 +0900

    feat(Lec6) : 예외처리
    1. try-catch 구문 역시 if-.. 와 마찬가지로 Expression 이다
    2. try-with-resource 는 .use() 함수를 통해 사용한다

---
+++ src/main/java/com/lannstark/lec07/Lec7.kt
@@ -0,0 +1,35 @@
+package com.lannstark.lec07
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+fun main() {
+    foo()
+}
+
+private fun parseInt(input: String): Int? {
+    return try {
+        input.toInt()
+    } catch (e: NumberFormatException) {
+        null
+    }
+}
+
+private fun foo(){
+    val currentFile = File(".")
+    val txt = File(currentFile.absolutePath + "/a.txt")
+    val bufferedReader = BufferedReader(FileReader(txt))
+    val txtLine = bufferedReader.readLine()
+    println(txtLine)
+    bufferedReader.close()
+}
+
+private fun use(){
+    val currentFile = File(".")
+    val txt = File(currentFile.absolutePath + "/a.txt")
+    val bufferedReader = BufferedReader(FileReader(txt))
+    bufferedReader.use {
+        reader -> println(reader.readLine())
+    }
+}
