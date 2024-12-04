73d8463ec1e52c06dae75fbd794035019ffddb59
calaf <117057567+ca1af@users.noreply.github.com>
2024-12-04 14:19:03 +0900

    feat : 코틀린의 람다

--- src/main/java/com/lannstark/lec17/Lec17.kt
+++ src/main/java/com/lannstark/lec17/Lec17.kt
@@ -3,6 +3,17 @@ package com.lannstark.lec17
 import java.util.*
 
 fun main() {
+    val fruits = fruits()
+    filter(fruits, isApple).forEach(::println)
+    filter(fruits) { it.name != "사과"}.forEach(::println)
+    fruits.filter { each -> each.name == "사과" }.mapNotNull {it.name}.forEach(::println)
+    booleanMethods(fruits)
+}
+
+private fun distinctBy(fruit: List<Fruit>): List<Fruit>{
+    return fruit.distinctBy { it.name }
+}
+private fun fruits(): List<Fruit> {
     val fruits = listOf(
         Fruit("사과", 1000),
         Fruit("사과", 1200),
@@ -13,9 +24,13 @@ fun main() {
         Fruit("바나나", 2500),
         Fruit("수박", 10000)
     )
+    return fruits
+}
 
-    filter(fruits, isApple).forEach(::println)
-    filter(fruits) { it.name != "사과"}.forEach(::println)
+private fun booleanMethods(fruits: List<Fruit>) {
+    fruits.all(isApple)
+    fruits.none(isApple)
+    fruits.any(isApple)
 }
 
 // 익명함수 형태 1
